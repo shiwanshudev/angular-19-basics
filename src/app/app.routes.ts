5e2d4a1ee627b847d4425abf62b5457f3b39e19d
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-11-25 20:33:59 +0530

    Add routes, user and admin component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
+import { AdminComponent } from './components/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'user-page', component: UserComponent },
+  { path: 'admin', component: AdminComponent },
+];
