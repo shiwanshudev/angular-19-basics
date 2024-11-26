15a860e2242d1cb6b863d70f6bcf050f87f25bf1
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-11-26 12:01:32 +0530

    Add data binding layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { DataBindingComponent } from './components/data-binding/data-binding.component';
 
 export const routes: Routes = [
   { path: 'user-page', component: UserComponent },
   { path: 'admin', component: AdminComponent },
+  { path: 'data-binding', component: DataBindingComponent },
 ];
