ddf92bc35d42600a70e7c062828d776a9baf8727
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-11-26 16:03:53 +0530

    Add click and onchange event

--- src/app/components/data-binding/data-binding.component.ts
+++ src/app/components/data-binding/data-binding.component.ts
@@ -4,8 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-data-binding',
   imports: [],
   templateUrl: './data-binding.component.html',
-  styleUrl: './data-binding.component.css'
+  styleUrl: './data-binding.component.css',
 })
 export class DataBindingComponent {
+  content: string = 'Hello World!';
+  text: string = '';
+  state: string = '';
 
+  displayText(event: Event) {
+    this.text = 'Working';
+  }
+
+  onChangeEvent(event: Event) {
+    this.state = (event.target as HTMLSelectElement).value;
+  }
 }
